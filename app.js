87012c4dd9ad2630fdc75789d87d4645e29fe6a4
tdtiger <pf529crm@s.okayama-u.ac.jp>
2025-04-24 16:08:16 +0900

    add page

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 const express = require("express");
 const app = express();
 
+// express-session(セッション管理)モジュールを読み込む
+const session = require("express-session");
+
 // fs(ファイルシステム)モジュールを読み込む
 const fs = require("fs");
 
@@ -15,12 +18,21 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({extended : true}));
 
+// セッションの設定
+app.use(session({
+    secret : 'quiz_secret',
+    resave : false,
+    saveUninitialized : true
+}));
+
 // クイズデータの読み込み
 const quizData = JSON.parse(fs.readFileSync("./questions.json", "utf-8"));
 
 // GETリクエストがホームページに送られた時の処理
 // クイズの表示ページに遷移する
 app.get("/", (req, res) => {
+    req.session.index = 0;
+    req.session.score = 0;
     const quiz = quizData[0];
     res.render("index", {quiz});
 });
@@ -28,9 +40,31 @@ app.get("/", (req, res) => {
 // POSTリクエストが回答ページに送られた時の処理
 app.post("/answer", (req, res) => {
     const userAnswer = req.body.answer;
-    const correct = quizData[0].answer === userAnswer;
-    res.render("result", {correct, answer : quizData[0].answer});    
+    const currentIndex = req.session.index;
+    const quiz = quizData[currentIndex];
+
+    // 正答かを判定
+    const correct = quiz.answer === userAnswer;
+    if(correct)
+        req.session.score += 1;
+
+    req.session.index += 1;
+
+    if(req.session.index >= quizData.length){
+        res.redirect("/finish");
+    }
+    else{
+        const nextQuiz = quizData[req.session.index];
+        res.render("index", {quiz : nextQuiz});
+    }  
 });
+
+// 終了画面への遷移
+app.get("/finish", (req, res) => {
+    const score = req.session.score;
+    const total = quizData.length;
+    res.render("finish", {score, total});
+})
 
 // 3000番ポートでアプリを起動
 app.listen(3000, () => {
